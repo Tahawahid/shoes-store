4202b2dfc3794f1434fa59e5bf67bdf6f2f2f8c1
Tahawahid <muhammadtahawahid1@gmail.com>
2023-08-18 19:38:07 +0500

    setup the project, import the assests, build a sami-navbar

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import CustomerReview from "./CustomerReview";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  Services,
+  PopularProducts,
+  CustomerReview,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
